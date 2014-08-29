597efa37d16b0c4a15fdbe6ca84b69ec68afe6cf
Wang Hao <eric.3126@hotmail.com>
2014-08-29 18:07:48 +0800

    Update test.html

--- src/localdb.coffee
+++ src/localdb.coffee
@@ -96,57 +96,7 @@ define (require, exports, module) ->
         collection = parse collection
         ls.setItem collectionName, stringify(c for c in collection when not criteriaCheck(c, criteria))
 
-
-    #Test
-    console.log "Current Keys:"
-    console.log (ls.key(i) for i in [0...ls.length])
-
-    console.log "Create new db:"
-    abc = new localDB("abc")
-    console.log abc
- 
-    console.log "Show Collections:"
-    console.log abc.collections()
- 
-    console.log "Clear collecionts:"
-    console.log abc.clear()
- 
-    console.log "Insert data in Collection 1"
-    abc.insert("collection1", {a:1,b:2,c:3})
-    abc.insert("collection1", {a:2,b:2,c:3})
-    abc.insert("collection1", {a:3,b:2,c:3})
-    abc.insert("collection1", {a:4,b:2,c:3})
-    abc.insert("collection1", {a:5,b:2,c:3})
-    abc.insert("collection1", {a:6,b:2,c:3})
-    abc.insert("collection1", {a:7,b:2,c:3})
-    abc.insert("collection1", {a:8,b:2,c:3})
-    abc.insert("collection1", {a:9,b:2,c:3})
-    abc.insert("collection1", {a:10,b:2,c:3})
-    abc.insert("collection1", {a:11,b:2,c:3})
-    abc.insert("collection1", {a:12,b:2,c:3})
-    abc.insert("collection2", {a:1,b:2,c:3})
-
-    console.log "Query data by criteria"
-    console.log abc.find("collection1",{a:{$gt:3,$lt:10}})
-
-    console.log "Query data by criteria and limit"
-    console.log abc.find("collection1",{a:{$gt:3,$lt:10}},4)
-
-    console.log "Update data by criteria and action"
-    abc.update("collection1", {$set:{b:4,c:5}} , {a:{$gt:3, $lt:10}})
-    console.log abc.find("collection1")
-
-    console.log "Delete data by criteria"
-    abc.remove("collection1", {a:{$gt:3,$lt:10}})
-    console.log abc.find("collection1")
-
-    console.log "Show Collections:"
-    console.log abc.collections()
-
-    #Test End    
-
     module.exports = localDB
-
 
     return
 
--- src/localdb.js
+++ src/localdb.js
@@ -18,7 +18,7 @@
  */
 define(function(require, exports, module) {
   'use strict';
-  var abc, criteriaCheck, dbPrefix, i, isArray, isFunction, isNumber, isObject, isString, isType, localDB, ls, parse, stringify, _isType;
+  var criteriaCheck, dbPrefix, isArray, isFunction, isNumber, isObject, isString, isType, localDB, ls, parse, stringify, _isType;
   ls = window.localStorage;
   dbPrefix = "ldb_";
   _isType = function(type) {
@@ -207,124 +207,5 @@ define(function(require, exports, module) {
       return _results;
     })()));
   };
-  console.log("Current Keys:");
-  console.log((function() {
-    var _i, _ref, _results;
-    _results = [];
-    for (i = _i = 0, _ref = ls.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-      _results.push(ls.key(i));
-    }
-    return _results;
-  })());
-  console.log("Create new db:");
-  abc = new localDB("abc");
-  console.log(abc);
-  console.log("Show Collections:");
-  console.log(abc.collections());
-  console.log("Clear collecionts:");
-  console.log(abc.clear());
-  console.log("Insert data in Collection 1");
-  abc.insert("collection1", {
-    a: 1,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 2,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 3,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 4,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 5,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 6,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 7,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 8,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 9,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 10,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 11,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection1", {
-    a: 12,
-    b: 2,
-    c: 3
-  });
-  abc.insert("collection2", {
-    a: 1,
-    b: 2,
-    c: 3
-  });
-  console.log("Query data by criteria");
-  console.log(abc.find("collection1", {
-    a: {
-      $gt: 3,
-      $lt: 10
-    }
-  }));
-  console.log("Query data by criteria and limit");
-  console.log(abc.find("collection1", {
-    a: {
-      $gt: 3,
-      $lt: 10
-    }
-  }, 4));
-  console.log("Update data by criteria and action");
-  abc.update("collection1", {
-    $set: {
-      b: 4,
-      c: 5
-    }
-  }, {
-    a: {
-      $gt: 3,
-      $lt: 10
-    }
-  });
-  console.log(abc.find("collection1"));
-  console.log("Delete data by criteria");
-  abc.remove("collection1", {
-    a: {
-      $gt: 3,
-      $lt: 10
-    }
-  });
-  console.log(abc.find("collection1"));
-  console.log("Show Collections:");
-  console.log(abc.collections());
   module.exports = localDB;
 });
