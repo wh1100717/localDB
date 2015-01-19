c63299bd89b3e226e1a3972d62bb93fc17dd6c29
卜一 <eric.wh@alibaba-inc.com>
2015-01-19 14:57:51 +0800

    update

--- dist/0.1.0/localdb-sea.js
+++ dist/0.1.0/localdb-sea.js
@@ -568,7 +568,7 @@ var Utils = (function(){
   return Utils;
 })();
 
-var Promise = Utils.isFunction(window.Promise) ? window.Promise : (function(){
+var Promise = (function(){
 
   /**
    * https://github.com/then/promise [v6.0.1]
